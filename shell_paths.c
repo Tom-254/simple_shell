97687211a569149763cca61a37fa19271e4db82a
Tom-254 <211mseeratiego@gmail.com>
2022-08-11 10:17:00 +0000

    mandatory tasks corrections

--- shell_paths.c
+++ shell_paths.c
@@ -94,21 +94,22 @@ int _isalpha(int c)
  */
 int _atoi(char *s)
 {
-	int i, j, n, x;
+	int res, sign, i;
 
-	i = n = 0;
-	x = 1;
-	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	res = 0;
+
+	sign = 1;
+
+	i = 0;
+
+	if (str[0] == '-')
 	{
-		if (s[i] == '-')
-			x *= -1;
+		sign = -1;
 		i++;
 	}
-	j = i;
-	while ((s[j] >= '0') && (s[j] <= '9'))
-	{
-		n = (n * 10) + x * ((s[j]) - '0');
-		j++;
-	}
-	return (n);
+
+	for (; str[i] != '\0'; ++i)
+		res = res * 10 + str[i] - '0';
+
+	return (sign * res);
 }
